ace20571dbcf7d6d9907f2f37372f1b53529568b
ItstepIgor <83533310+ItstepIgor@users.noreply.github.com>
2022-05-17 21:39:48 +0300

    fix path

--- src/main/java/com/springmessenger/service/MessageService.java
+++ src/main/java/com/springmessenger/service/MessageService.java
@@ -5,26 +5,31 @@ import com.springmessenger.entity.Message;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class MessageService {
 
-//    @Autowired
+    //    @Autowired
 //    private ColumnPositionMappingStrategy columnPositionMappingStrategy;
+
 
     public List<Message> getAll() {
         List<Message> messages;
 
         try {
-            Path path = Path.of("K:\\Java\\IdeaProjects\\spring-messenger\\src\\main\\resources\\messages.csv");
-            Reader reader = Files.newBufferedReader(path);
+            URL resource = getClass().getClassLoader().getResource("messages.csv");
+            File file = Paths.get(resource.toURI()).toFile();
 
-            messages = new CsvToBeanBuilder(reader)
-                    .withType(Message.class).build().parse();
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                messages = new CsvToBeanBuilder(reader)
+                        .withType(Message.class).build().parse();
+            }
 
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -48,13 +53,18 @@ public class MessageService {
     }
 
     private void saveToFile(List<Message> messages) {
-        Path path = Path.of("K:\\Java\\IdeaProjects\\spring-messenger\\src\\main\\resources\\messages.csv");
-        try (Writer writer = new FileWriter(path.toString())) {
-            StatefulBeanToCsv beanToCsv = new StatefulBeanToCsvBuilder(writer).build();
-            beanToCsv.write(messages);
+//        Path path = Path.of("E:\\JAVA\\IdeaProjects\\spring-messenger\\src\\main\\resources\\messages.csv");
+        try {
+            URL resource = getClass().getClassLoader().getResource("messages.csv");
+            File file = Paths.get(resource.toURI()).toFile();
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+                StatefulBeanToCsv beanToCsv = new StatefulBeanToCsvBuilder(writer).build();
+                beanToCsv.write(messages);
+            }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+
     }
 
     public void update(long id, String text) {
