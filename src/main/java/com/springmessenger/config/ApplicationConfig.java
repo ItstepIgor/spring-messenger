3e94231d2bea06e3589210a584877e4569097b20
ItstepIgor <83533310+ItstepIgor@users.noreply.github.com>
2022-05-20 12:56:58 +0300

    Add translation

--- src/main/java/com/springmessenger/config/ApplicationConfig.java
+++ src/main/java/com/springmessenger/config/ApplicationConfig.java
@@ -1,17 +1,38 @@
 package com.springmessenger.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 @Configuration
 @ComponentScan(basePackages = "com.springmessenger")
 public class ApplicationConfig {
 
+
     @Bean
     public Scanner scanner() {
         return new Scanner(System.in);
+    }
+
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("translation");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public PrintStream printStream() {
+        return new PrintStream(System.out, true, StandardCharsets.UTF_8);
     }
 }
--- src/main/java/com/springmessenger/controller/InputController.java
+++ src/main/java/com/springmessenger/controller/InputController.java
@@ -1,8 +1,11 @@
 package com.springmessenger.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 
+import java.io.PrintStream;
+import java.util.Locale;
 import java.util.Scanner;
 
 @Controller
@@ -13,34 +16,50 @@ public class InputController {
     @Autowired
     private Scanner scanner;
 
+    @Autowired
+    private MessageSource messageSource;
+    @Autowired
+    private PrintStream printStream;
+
     public void selectAction() {
+        Locale locale = null;
         System.out.println("""
-                Выберите действие:
-                1 - Вывести список сообщений
-                2 - Создать новое сообщение
-                3 - Отредактировать сообщение
-                4 - Удалить сообщение
+                1 - English
+                2 - Русский
                 """);
-        int a = scanner.nextInt();
-        switch (a) {
+        int selectLanguage = scanner.nextInt();
+        switch (selectLanguage) {
+            case 1 -> locale = new Locale("en", "EN");
+            case 2 -> locale = new Locale("ru", "RU");
+        }
+
+        printStream.println(messageSource.getMessage("select.action", null, locale));
+        printStream.println("1 - " + messageSource.getMessage("select.list.message", null, locale));
+        printStream.println("2 - " + messageSource.getMessage("create.message", null, locale));
+        printStream.println("3 - " + messageSource.getMessage("edit.message", null, locale));
+        printStream.println("4 - " + messageSource.getMessage("delete.message", null, locale));
+        int select = scanner.nextInt();
+        switch (select) {
             case 1 -> messageController.getAll();
             case 2 -> {
-                System.out.println("Введите имя");
+                printStream.println(messageSource.getMessage("enter.name", null, locale));
                 String name = scanner.next();
-                System.out.println("Введите текст сообщения");
+                printStream.println(messageSource.getMessage("enter.text", null, locale));
                 String text = scanner.next();
                 messageController.create(name, text);
             }
             case 3 -> {
-                System.out.println("Выберите id сообщения для изменения");
+                printStream.println(messageSource.getMessage("select.id.message.edit", null, locale));
+                System.out.println();
                 messageController.getAll();
                 long id = scanner.nextInt();
-                System.out.println("Введите новый текст сообщения");
+                printStream.println(messageSource.getMessage("enter.new.text", null, locale));
                 String text = scanner.next();
                 messageController.update(id, text);
             }
             case 4 -> {
-                System.out.println("Выберите id сообщения для удаления");
+                printStream.println(messageSource.getMessage("select.id.message.delete", null, locale));
+                System.out.println();
                 messageController.getAll();
                 long id = scanner.nextInt();
                 messageController.delete(id);
