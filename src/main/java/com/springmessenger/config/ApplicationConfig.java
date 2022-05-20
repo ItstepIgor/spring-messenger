d1be8eb0ccbe808798cdbe0b8c275a755ab391d6
ItstepIgor <83533310+ItstepIgor@users.noreply.github.com>
2022-05-20 13:01:46 +0300

    Add translation

--- src/main/java/com/springmessenger/config/ApplicationConfig.java
+++ src/main/java/com/springmessenger/config/ApplicationConfig.java
@@ -1,12 +1,10 @@
 package com.springmessenger.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
