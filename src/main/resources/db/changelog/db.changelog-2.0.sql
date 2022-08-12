10e17a827a3c76912e22d833381cb3606f147261
ItstepIgor <83533310+ItstepIgor@users.noreply.github.com>
2022-08-12 16:56:41 +0300

    Add liquibase. Don't work

---
+++ src/main/resources/db/changelog/db.changelog-2.0.sql
@@ -0,0 +1,8 @@
+--liquibase formatted sql
+
+--changeset igor:1
+ALTER TABLE IF EXISTS messages ADD CONSTRAINT FK_messages_users FOREIGN KEY (sender_user_id) REFERENCES users;
+
+ALTER TABLE IF EXISTS chats_users ADD CONSTRAINT FK_chats_users_users FOREIGN KEY (user_id) REFERENCES users;
+
+ALTER TABLE IF EXISTS chats_users ADD CONSTRAINT FK_chats_users_chats FOREIGN KEY (chat_id) REFERENCES chats;
