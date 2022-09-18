3c3f53ae0262ad2764368164265540c281f1c58a
ItstepIgor <83533310+ItstepIgor@users.noreply.github.com>
2022-09-18 19:51:11 +0300

    Add new test

--- src/test/java/com/springmessenger/service/MessageServiceTest.java
+++ src/test/java/com/springmessenger/service/MessageServiceTest.java
@@ -5,6 +5,7 @@ import com.springmessenger.repository.MessageRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -21,22 +22,44 @@ class MessageServiceTest {
     @InjectMocks
     private MessageService messageService;
 
+    Message message = Message.builder().setId(MESSAGE_ID).build();
+
     @Test
     void findById() {
-//        Mockito.doReturn(Message.builder()
-//                        .setId(MESSAGE_ID)
-//                        .build())
-//                .when(messageRepository.findById(MESSAGE_ID));
 
-        Mockito.when(messageRepository.findById(MESSAGE_ID)).thenReturn(Message.builder()
-                .setId(MESSAGE_ID)
-                .build());
+//      второй способ для мока не работает (разобраться если будет время)
+//        Mockito.doReturn(Message.builder().setId(MESSAGE_ID).build()).when(messageRepository.findById(MESSAGE_ID));
+        Mockito.when(messageRepository.findById(MESSAGE_ID)).thenReturn(message);
         Message actualResult = messageService.findById(MESSAGE_ID);
 
-        Message expectedResult = Message.builder()
-                .setId(MESSAGE_ID)
-                .build();
-
+        Message expectedResult = message;
+        ArgumentCaptor<Long> argumentCaptor = ArgumentCaptor.forClass(Long.class);
+        Mockito.verify(messageRepository).findById(argumentCaptor.capture());
+        //Сравниваем результат из метода
         Assertions.assertEquals(expectedResult, actualResult);
+        //Сравниваем значение аргумента пришедшеее в метод с отправленным
+        Assertions.assertEquals(MESSAGE_ID, argumentCaptor.getValue());
     }
+
+    @Test
+    void deleteMessage() {
+        Mockito.doReturn(true).when(messageRepository.delete(message));
+//        Mockito.when(messageRepository.delete(message))
+//                .thenReturn(true)
+//                .thenReturn(false);
+    }
+
+    @Test
+    void getAllMessage() {
+    }
+
+    @Test
+    void saveMessage() {
+    }
+
+    @Test
+    void updateMessage() {
+    }
+
+
 }
