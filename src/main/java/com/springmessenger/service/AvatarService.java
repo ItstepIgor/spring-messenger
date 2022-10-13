d2116cd71d507cf33793b55daa49f5369bc1ac0f
ItstepIgor <83533310+ItstepIgor@users.noreply.github.com>
2022-10-13 20:53:37 +0300

    check AvatarService

--- src/main/java/com/springmessenger/service/AvatarService.java
+++ src/main/java/com/springmessenger/service/AvatarService.java
@@ -9,7 +9,6 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.gridfs.GridFsOperations;
 import org.springframework.data.mongodb.gridfs.GridFsResource;
-import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,30 +18,31 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class AvatarService {
 
-
-//    private final AvatarRepository avatarRepository;
-
-    private final GridFsTemplate gridFsTemplate;
-
     private final GridFsOperations operations;
 
     public String addAvatar(String title, MultipartFile file) throws IOException {
         DBObject metaData = new BasicDBObject();
         metaData.put("type", "image");
         metaData.put("title", title);
-        ObjectId id = gridFsTemplate.store(file.getInputStream(), title, file.getContentType(), metaData);
+        ObjectId id = operations.store(file.getInputStream(), title, file.getContentType(), metaData);
         return id.toString();
     }
 
     public GridFsResource getAvatar(String id) throws IllegalStateException {
-        GridFSFile file = gridFsTemplate.findOne(new Query(Criteria.where("_id").is(id)));
+        // TODO "_id" - constant
+        // TODO null checks (idea warnings)
+        // TODO save content type to metadata for put it to get method header
+        // TODO return object with file data from service, replace GridFsResource with InputStream
+        //  return value from controller - ResponseEntity<InputStreamResource>
+
 //        Avatar avatar = new Avatar();
 //        avatar.setTitle(file.getMetadata().get("title").toString());
 //        avatar.setImage(operations.getResource(file).getInputStream());
+        GridFSFile file = operations.findOne(new Query(Criteria.where("_id").is(id)));
         return operations.getResource(file);
     }
 
     public void delete(String id) {
-        gridFsTemplate.delete(new Query(Criteria.where("_id").is(id)));
+        operations.delete(new Query(Criteria.where("_id").is(id)));
     }
 }
