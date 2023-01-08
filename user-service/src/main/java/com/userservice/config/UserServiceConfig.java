8d7ccb11402a0dd03000c3bf7a60c9e78b176fda
ItstepIgor <83533310+ItstepIgor@users.noreply.github.com>
2023-01-08 21:47:27 +0300

    Added authorization-service

--- user-service/src/main/java/com/userservice/config/UserServiceConfig.java
+++ user-service/src/main/java/com/userservice/config/UserServiceConfig.java
@@ -1,8 +1,11 @@
 package com.userservice.config;
 
 import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
@@ -10,12 +13,27 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @ComponentScan(basePackages = "com.userservice")
 @EnableAspectJAutoProxy
 @EnableTransactionManagement
+@EnableWebSecurity
 public class UserServiceConfig {
 
+
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        PasswordEncoder encoder = new BCryptPasswordEncoder();
-        return encoder;
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .mvcMatcher("/api/users/**")
+                .authorizeRequests()
+                .mvcMatchers("/api/users/**")
+                .access("hasAuthority('SCOPE_resource.read')")
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
     }
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        PasswordEncoder encoder = new BCryptPasswordEncoder();
+//        return encoder;
+//    }
 
 }
